8625b7278e3f1d5985c7a5d656dce733279a8b50
hjasier <asiaurre@gmail.com>
2024-11-30 13:05:20 +0100

    send gy85 to serial esp82

--- ESP8266/DroneWifiController/src/main.cpp
+++ ESP8266/DroneWifiController/src/main.cpp
@@ -10,6 +10,7 @@ int number = 0;
 
 void webSocketEvent(uint8_t num, WStype_t type, uint8_t *payload, size_t length);
 void processMessage(String message);
+void processSerialMessage(String message);
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
@@ -44,7 +45,8 @@ void loop() {
     String serialMessage = Serial.readStringUntil('\n'); // Leer hasta el final de la línea
     serialMessage.trim(); // Eliminar espacios en blanco iniciales y finales
     if (serialMessage.length() > 0) {
-      Serial.println("Mensaje recibido por Serial: " + serialMessage);
+      processSerialMessage(serialMessage);
+      //Serial.println("Mensaje recibido por Serial: " + serialMessage);
     }
   }
 
@@ -92,4 +94,78 @@ void processMessage(String message) {
   } else if (message == "OFF") {
     digitalWrite(LED_BUILTIN, HIGH);
   }
+}
+
+
+void processSerialMessage(String message) {
+    // Variables para almacenar los datos extraídos
+    int16_t GYX = 0, GYY = 0, GYZ = 0;
+    int16_t MAGX = 0, MAGY = 0, MAGZ = 0;
+    int16_t ACX = 0, ACY = 0, ACZ = 0;
+
+    // Buscar y extraer cada dato usando índices
+    int startIdx, endIdx;
+
+    // GY85 Data
+    startIdx = message.indexOf("GYX:");
+    if (startIdx != -1) {
+        endIdx = message.indexOf(" ", startIdx);
+        GYX = message.substring(startIdx + 4, endIdx).toInt();
+    }
+    startIdx = message.indexOf("GYY:");
+    if (startIdx != -1) {
+        endIdx = message.indexOf(" ", startIdx);
+        GYY = message.substring(startIdx + 4, endIdx).toInt();
+    }
+    startIdx = message.indexOf("GYZ:");
+    if (startIdx != -1) {
+        endIdx = message.indexOf("\r\n", startIdx);
+        GYZ = message.substring(startIdx + 4, endIdx).toInt();
+    }
+
+    // HMC5883L Data
+    startIdx = message.indexOf("MAGX:");
+    if (startIdx != -1) {
+        endIdx = message.indexOf(" ", startIdx);
+        MAGX = message.substring(startIdx + 5, endIdx).toInt();
+    }
+    startIdx = message.indexOf("MAGY:");
+    if (startIdx != -1) {
+        endIdx = message.indexOf(" ", startIdx);
+        MAGY = message.substring(startIdx + 5, endIdx).toInt();
+    }
+    startIdx = message.indexOf("MAGZ:");
+    if (startIdx != -1) {
+        endIdx = message.indexOf("\r\n", startIdx);
+        MAGZ = message.substring(startIdx + 5, endIdx).toInt();
+    }
+
+    // ADXL345 Data
+    startIdx = message.indexOf("ACX:");
+    if (startIdx != -1) {
+        endIdx = message.indexOf(" ", startIdx);
+        ACX = message.substring(startIdx + 4, endIdx).toInt();
+    }
+    startIdx = message.indexOf("ACY:");
+    if (startIdx != -1) {
+        endIdx = message.indexOf(" ", startIdx);
+        ACY = message.substring(startIdx + 4, endIdx).toInt();
+    }
+    startIdx = message.indexOf("ACZ:");
+    if (startIdx != -1) {
+        endIdx = message.indexOf("\r\n", startIdx);
+        ACZ = message.substring(startIdx + 4, endIdx).toInt();
+    }
+
+    // Imprimir los datos procesados
+    Serial.println("Datos Procesados:");
+    Serial.print("GYX: "); Serial.println(GYX);
+    Serial.print("GYY: "); Serial.println(GYY);
+    Serial.print("GYZ: "); Serial.println(GYZ);
+    Serial.print("MAGX: "); Serial.println(MAGX);
+    Serial.print("MAGY: "); Serial.println(MAGY);
+    Serial.print("MAGZ: "); Serial.println(MAGZ);
+    Serial.print("ACX: "); Serial.println(ACX);
+    Serial.print("ACY: "); Serial.println(ACY);
+    Serial.print("ACZ: "); Serial.println(ACZ);
 }
