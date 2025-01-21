5459bf94fcf921464ed23155f5b3652681feaea2
Javier Santamaría Pascual <145108598+javiers2004@users.noreply.github.com>
2025-01-21 12:01:26 +0100

    Update main.c

--- STM32F303/FlightController/Core/Src/main.c
+++ STM32F303/FlightController/Core/Src/main.c
@@ -113,7 +113,7 @@ int main(void)
 
   /* USER CODE BEGIN SysInit */
 
-  /* USER CODE END SysInit */
+  /* USER CODE END SysInit */ 
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
